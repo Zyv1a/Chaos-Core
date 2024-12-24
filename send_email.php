96bbfaabd2482b4489674ca548508d7b876fb356
Zyv1a <110346397+Zyv1a@users.noreply.github.com>
2024-12-24 10:54:25 +0000

    Update

--- send_email.php
+++ send_email.php
@@ -1,28 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
-    $category = htmlspecialchars($_POST['category']);
-    $question = htmlspecialchars($_POST['question']);
+    $form_type = htmlspecialchars($_POST['form_type']);
     $email = htmlspecialchars($_POST['email']);
 
-    // Définir l'adresse email où envoyer le formulaire
-    $to = "votre-email@example.com"; // Modifiez cette ligne avec l'email souhaité
-    $subject = "Nouvelle question de la catégorie: $category";
+    if ($form_type == 'recrutement') {
+        $role = htmlspecialchars($_POST['role']);
+        $experience = htmlspecialchars($_POST['experience']);
+        $subject = "Recrutement - $role";
+        $message = "Rôle recherché: $role\nExperience: $experience\nEmail: $email";
+    } elseif ($form_type == 'suggestion_bug') {
+        $issue_type = htmlspecialchars($_POST['issue_type']);
+        $details = htmlspecialchars($_POST['details']);
+        $subject = "$issue_type soumis";
+        $message = "$issue_type: $details\nEmail: $email";
+    }
 
-    // Créer le message
-    $message = "
-    Catégorie: $category\n
-    Question: $question\n
-    Email: $email
-    ";
-
-    // Définir les en-têtes
+    $to = "votre-email@example.com"; // Remplacez par l'email de destination
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
-    // Envoyer l'email
     if (mail($to, $subject, $message, $headers)) {
-        echo "Merci de votre soumission. Votre message a été envoyé.";
+        echo "Merci pour votre soumission. Votre message a été envoyé.";
     } else {
         echo "Une erreur est survenue lors de l'envoi du message.";
     }
